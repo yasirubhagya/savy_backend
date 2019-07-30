917c60668bbaeddfe13bef93468b08ff10bc2e94
yasirubhagya <yasirubhagya@gmail.com>
2019-07-30 14:36:02 +0530

    2nd

--- api/routes/gps.js
+++ api/routes/gps.js
@@ -9,9 +9,9 @@ const gpsController = require('../controllers/gpsController');
 gpsRouter.get('/getAll',gpsController.gps_get_all);
 
 
-gpsRouter.get('/getById',gpsController.gps_get_byTrackerId);
+gpsRouter.get('/getById/:Id',gpsController.gps_get_byTrackerId);
 
-gpsRouter.get('/getById/live',gpsController.gps_get_live_byTrackerId);
+gpsRouter.get('/getById/:Id/live',gpsController.gps_get_live_byTrackerId);
 
 gpsRouter.get('/gps.json/:trackerId/:uDate/:latitude/:longitude/:speed/:battery',gpsController.gps_add);
 
--- mainroutes.js
+++ mainroutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
 const gpsRoutes = require('./api/routes/gps');
+
 router.use('/trackers',gpsRoutes);
 module.exports = router;
